cfc3189b5fde45c40306f712487f4b6345999694
Mauricio Giordano <mauricio.c.giordano@gmail.com>
2014-07-08 18:56:21 +0100

    Added create from CRUD

--- test/controllers/user.js
+++ test/controllers/user.js
@@ -21,7 +21,7 @@ exports.getUsers = function(req, res)
 	var status = 200;
 	var format = 'html';
 
-	var users;
+	var users = null;
 
 	async.series([
 
@@ -49,6 +49,111 @@ exports.getUsers = function(req, res)
 			res.status(status).send(users);
 		else
 			res.render('getUsers', { data : users });
+	});
+};
+
+/*
+ * [POST] CREATE AN ENTRY FOR User
+ *
+ * @param String name
+ * @param String username
+ * @param String email
+ * @param String password
+ * @param Number age
+ *
+ * @return User user
+ */
+exports.postUser = function(req, res)
+{
+	var data = req.body;
+
+	var status = 200;
+	var format = 'html';
+
+	var user = null;
+
+	async.series([
+
+		function(callback)
+		{
+			if(data == null)
+			{
+				status = 400;
+				callback(true);
+			}
+			else if(data.name === undefined)
+			{
+				status = 400;
+				callback(true);
+			}
+			else if(data.username === undefined)
+			{
+				status = 400;
+				callback(true);
+			}
+			else if(data.email === undefined)
+			{
+				status = 400;
+				callback(true);
+			}
+			else if(data.password === undefined)
+			{
+				status = 400;
+				callback(true);
+			}
+			else if(data.age === undefined)
+			{
+				status = 400;
+				callback(true);
+			}
+			else 
+			{
+				callback();
+			}
+		},
+		function(callback)
+		{
+			User
+			.findOne({
+				username : data.username,
+				email : data.email
+			})
+			.exec(function(err, retData))
+			{
+				if(retData != null)
+				{
+					status = 400;
+					callback(true);
+				}
+				else
+				{
+					callback();
+				}
+			});
+		},
+		function(callback)
+		{
+			user = new User({
+				name = data.name,
+				username = data.username,
+				email = data.email,
+				password = data.password,
+				age = data.age,
+			});
+
+			user.save(function(err, retData)
+			{
+				user = retData;
+
+				callback();
+			});
+		}
+	], function(invalid)
+	{
+		if(format == 'json')
+			res.status(status).send(user);
+		else
+			res.render('postUser', { data : user });
 	});
 };
 
--- test/models/user.js
+++ test/models/user.js
@@ -4,6 +4,7 @@ var mongoose = require('mongoose')
 var UserSchema = mongoose.Schema({
 	name: String,
 	username: String,
+	email: String,
 	password: String,
 	age: Number,
 	devices: [{
